6bc0ea6dae3301c71b128bddac7afc260227b4cc
anasofiabeltran <anas.beltran206@gmail.com>
2022-07-20 15:39:29 -0500

    asincronismo y conteo productos

--- js/API.js
+++ js/API.js
@@ -1,18 +1,22 @@
+import { add,less } from "./index.js";
+
 const URL = "https://pokeapi.co/api/v2/pokemon/"
 
-function cargarSeccionProductosContenido(){
-    const seccionProductosContenido = document.querySelector('#section-products_content');
-    const seccion1 = document.createElement("section")
+function crearProducto(seccion,numeroInicio){
     for(let i=1; i<9; i++){
-        fetch(`${URL}${i}`)
+        fetch(`${URL}${numeroInicio+i}`)
         .then(respuesta => respuesta.json())
         .then(data => {
+            
             const div = document.createElement("div");
             const img = document.createElement("img");
             const divInfoCart = document.createElement("div");
             const h5 = document.createElement("h5");
             const p = document.createElement("p");
             const divBotones = document.createElement("div");
+            const botonSumar = document.createElement("button");
+            const cantidadInput = document.createElement("input");
+            const botonRestar = document.createElement("button");
 
             div.className = "card";
             img.src = data.sprites.front_default;
@@ -22,55 +26,45 @@ function cargarSeccionProductosContenido(){
             h5.textContent=data.name;
             p.className = "card-text";
             p.textContent = "Some quick example text to build on the card title and make up the bulk of the card's content";
+            botonSumar.onclick = () => add(data.name);
+            botonSumar.textContent = "+";
+            cantidadInput.type = "text";
+            cantidadInput.id=`quantity_of_products_${data.name}`;
+            cantidadInput.value="0";
+            botonRestar.onclick = () =>less(data.name);
+            botonRestar.textContent="-";
 
-
+            divBotones.append(botonSumar,cantidadInput,botonRestar);
             divInfoCart.append(h5,p,divBotones);
             div.append(img,divInfoCart);
-            seccion1.append(div);
-
+            seccion.append(div);
         })
     }
+}
+
+function cargarSeccionProductosContenido(){
+    const seccionProductosContenido = document.querySelector('#section-products_content');
+    const seccion1 = document.createElement("section")
+    crearProducto(seccion1,200);
     seccion1.id="section1";
     seccionProductosContenido.append(seccion1);
 
 
 
     const seccion2 = document.createElement("section")
-    for(let i=1; i<9; i++){
-        fetch(`${URL}${i}`)
-        .then(respuesta => respuesta.json())
-        .then(data => {
-            const div = document.createElement("div");
-            const img = document.createElement("img");
-            const divInfoCart = document.createElement("div");
-            const h5 = document.createElement("h5");
-            const p = document.createElement("p");
-            const divBotones = document.createElement("div");
-
-            div.className = "card";
-            img.src = data.sprites.front_default;
-            img.className = "card-img-top";
-            divInfoCart.className ="card-body";
-            h5.className="card-title";
-            h5.textContent=data.name;
-            p.className = "card-text";
-            p.textContent = "Some quick example text to build on the card title and make up the bulk of the card's content";
-
-
-            divInfoCart.append(h5,p,divBotones);
-            div.append(img,divInfoCart);
-            seccion1.append(div);
-
-        })
-    }
+    crearProducto(seccion2,150);
     seccion2.id="section2";
     seccionProductosContenido.append(seccion2);
+
 }
+
+
+
 
 function cargarCategoriasContainerImagenes(){
     const categoriasContainerImagenes = document.querySelector('#categories-container_imagenes');
     for(let i=1; i<4; i++){
-        fetch(`${URL}${i}`)
+        fetch(`${URL}${120+i}`)
         .then(respuesta => respuesta.json())
         .then(data => {
             const div = document.createElement("div");
--- js/index.js
+++ js/index.js
@@ -1,45 +1,9 @@
-let products = {
-    chair:0,
-    phone:0,
-    headphones:0,
-}
+/*
 
 let productInfomation = {
     chair: ["../assets/img/furniture-modern-studio-lifestyle-green.jpg"],
     phone: ["../assets/img/smartphone-with-blank-black-screen-innovative-future-technology.jpg"],
     headphones: ["../assets/img/pink-headphones-wireless-digital-device.jpg"],
-}
-
-function listOfProducts(){
-    let p=Object.values(products);
-    let add=(a,b) => a+b;
-    let total = p.reduce(add);
-    let product = document.getElementById("total");
-    product.innerText = `${total}`;
-}
-
-function start(){
-    let productChair = document.getElementById("quantity_of_products_chair");
-    let productPhone = document.getElementById("quantity_of_products_phone");
-    let productheadphones = document.getElementById("quantity_of_products_headphones");
-    productChair.value = "0";
-    productPhone.value = "0";
-    productheadphones.value = "0";
-}
-
-
-function add(product){
-    products[product]+=1;
-    let p = document.getElementById(`quantity_of_products_${product}`);
-    p.value =  `${products[product]}`;
-    listOfProducts();
-}
-
-function less(product){
-    products[product]-=1;
-    let p = document.getElementById(`quantity_of_products_${product}`);
-    p.value =  `${products[product]}`;
-    listOfProducts();
 }
 
 function uploadProducts(){
@@ -61,7 +25,55 @@ function uploadProducts(){
     
 }
 
-
 uploadProducts();
 listOfProducts();
-start();
+
+*/
+let products = {
+}
+
+
+function listOfProducts(){
+    let p=Object.values(products);
+    let add=(a,b) => a+b;
+    let total = p.reduce(add);
+    let product = document.getElementById("total");
+    product.innerText = `${total}`;
+}
+
+
+export function add(product){
+    let resultado = Object.keys(products).find((p) => {
+        return(p ==product)
+    });
+    if(resultado == undefined){
+        products[product]=1;
+        let p = document.getElementById(`quantity_of_products_${product}`);
+        p.value =  `${products[product]}`;
+    }else if (resultado){
+        products[product]+=1;
+        let p = document.getElementById(`quantity_of_products_${product}`);
+        p.value =  `${products[product]}`;
+    }
+    console.log(products);
+
+    listOfProducts();
+
+
+}
+
+export function less(product){
+
+    let resultado = Object.keys(products).find((p) => {
+        return(p ==product)
+    });
+    if (resultado){
+        products[product]-=1;
+        let p = document.getElementById(`quantity_of_products_${product}`);
+        p.value =  `${products[product]}`;
+    }
+    console.log(products);
+    
+    listOfProducts();
+
+}
